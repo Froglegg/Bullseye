4ecda44ad86c72379999491a99db46ad840d1d91
Hayes Crowley <froglegg@MacBook-Pro.local>
2020-11-29 12:47:25 -0500

    layout complete

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -8,49 +8,70 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var alertIsVisible:Bool = false
-    @State var showKnockKnock:Bool = false
+    @State var alertIsVisible = false
+    @State var sliderValue = 50.0
+    @State var target = Int.random(in: 1...100)
     
     var body: some View {
-        
-        
-        VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/, content: {
-            Text("Welcome to my first Swift App!").fontWeight(.semibold).foregroundColor(.green)
-            
+        VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: nil, content: {
+            // target row
+            Spacer()
+            HStack {
+                Text("Put the bullseye as close as you can to:")
+                Text("\(target)")
+            }
+            Spacer()
+            // slider row
+            HStack{
+                Text("1")
+                Slider(value: $sliderValue, in: 1...100)
+                Text("100")
+            }
+            Spacer()
+            // hit me button row
             Button(action: {print("button pressed")
-                
-                self.alertIsVisible = true
+                alertIsVisible = true
             })
             {
                 Text("Hit me")
             }.alert(isPresented: $alertIsVisible, content: {
-                Alert (title: Text("Hello there!"), message: Text("This is my first alert!"), dismissButton: .default(Text("Awesome!")))
+                Alert (title: Text("Hello there!"), message: Text("The slider value is \(sliderValueRounded()).\n" + "You scored \(pointsForCurrentRound()) points this round."), dismissButton: .default(Text("Awesome!")))
             })
-            
-            Button(action: {
+            Spacer()
+         // score row
+            HStack{
+                Button(action: {print("button pressed!")}, label: {
+                    Text("Start Over")
+                })
+                Spacer()
+                Text("Score: ")
+                Text("9999999")
+                Spacer()
+                Text("Round: ")
+                Text("999")
+                Spacer()
+                Button(action:{print("button pressed!")}, label: {
+                    Text("Info")
+                })
                 
-                self.showKnockKnock = true
-            })
-            {
-                Text("Knock Knock")
-            }.alert(isPresented: $showKnockKnock, content: {
-                Alert (title: Text("Who's there?"), message: Text("Some knock knock joke"), dismissButton: .default(Text("huh")))
-            })
+            }.padding(.bottom, 20)
             
-//            Button(action: {
-//                self.showKnockKnock = true
-//            }){
-//                Text("Knock Knock"))
-//            }
+            
         })
+        
+    }
+    func sliderValueRounded() -> Int{
+        return Int(sliderValue.rounded())
+    }
+    func pointsForCurrentRound()-> Int {
+      return 100 - abs((target - sliderValueRounded()))
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-      ContentView()
+            ContentView().previewLayout(.fixed(width: 896, height: 414))
         }
-            
     }
 }
